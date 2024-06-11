26d5e75b24300521a406c5c6dc1def5279093e93
EyalZusiman <eyal.zusiman@gmail.com>
2024-06-11 22:49:41 +0300

    update Sample

--- Sample.cs
+++ Sample.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -19,5 +20,19 @@ internal class Sample
             anm = AnmFile.CreateFrom(file);
         using (FileStream outFile = new(outputPath, FileMode.Create, FileAccess.Write))
             JsonSerializer.Serialize(outFile, anm, options);
+    }
+
+    public static void JsonToAnm(string jsonPath, string outputPath)
+    {
+        JsonSerializerOptions options = new()
+        {
+            NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals,
+            WriteIndented = true,
+        };
+        AnmFile anm;
+        using (FileStream file = new(jsonPath, FileMode.Open, FileAccess.Read))
+            anm = JsonSerializer.Deserialize<AnmFile>(file, options) ?? throw new Exception("bad json format");
+        using (FileStream outFile = new(outputPath, FileMode.Create, FileAccess.Write))
+            anm.WriteTo(outFile);
     }
 }
